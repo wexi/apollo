49f3af343018435362b3969088f7c0f75c51040c
Enoch <ixew@hotmail.com>
2026-03-04 12:54:40 +0200

    Stop tracking local config and add sanitized template

--- local.example.py
+++ local.example.py
@@ -4,9 +4,9 @@ APOLLO = '/api/list/YOUR_EMAIL/PASSWORD/m3u8/'
 
 # TV stations of interest:
 TVLIVE = (r'(FOX News US)$', r'(FOX Business Network)$',
-          r'(NewsMax TV)$', r'(One America News Network)$',r'(NewsNation \(East\))$',
+          r'(NewsMax TV)$', r'(One America News Network)$', r'(NewsNation \(East\))$',
           r'(BBC World North America US)$', r'(sky.news.uk.png)', r'(i24)$',
-          r'(National Geographic US)', r'(Science US)$', 
+          r'(National Geographic US)', r'(Science US)$',
           r'(Channel 11 Kan)$', r'(Channel 12 Keset)$',
           r'(Channel 13 Reshet)$', r'(Channel 14)$',
           r'(Knesset)$', r'(music.il.png)')
